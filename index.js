9f14190918f5ee83d5d7a07795c9204bbb7dc45b
Aziz Kosber <a.kosber@gmail.com>
2018-03-25 00:50:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,10 +69,12 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if(!cardNumber) {
-    console.log("Sorry, we don't have a card on file for you.");
+  if (!cardNumber) {
+    console.log("Sorry, we don't have a credit card on file for you.");
     return false;
   }
+
   console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+
   cart = [];
 }
