c87594db49b1ac679b598caa49ac3de84d1632da
Aziz Kosber <a.kosber@gmail.com>
2018-03-25 00:26:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,30 @@ function addToCart(itemName) {
 
 
 function viewCart() {
-  // write your code here
+  let l = cart.length;
+  if(!l) {
+    return console.log("Your cart is empty.")
+  }
+  
+  let itemsAndPrices = [];
+  for(let i = 0; i < l; i++) {
+    let itemAndPrice = cart[i];
+    let item = Object.keys(itemAndPrice)[0];
+    let price = itemAndPrice[item];
+    itemsAndPrices.push(`${item} at \$${price}`);
+}
+  switch(itemsAndPrices.length) {
+    case 1:
+      break;
+    case 2:
+      itemsAndPrices = itemsAndPrices.join(" and ")
+      break;
+    default:
+      itemsAndPrices[l-1] = "and ".concat(itemsAndPrices[l-1]);
+                                          itemsAndPrices.join(", ");
+  }
+  console.log(`In your cart, you have ${itemsAndPrices}.`);
+}
 }
 
 function total() {
